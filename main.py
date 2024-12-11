cfb8465e6de0fe1c364cc8812d46df7137033b59
igeddes <iaingeddes@gmail.com>
2024-12-11 18:07:35 +0000

    Conclusion of step 3

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import pygame
-from constants import SCREEN_WIDTH, SCREEN_HEIGHT
+from constants import SCREEN_WIDTH, SCREEN_HEIGHT, MAX_FPS
 
 def main():
     print("Starting asteroids!")
@@ -10,10 +10,13 @@ def main():
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     while True:
         pygame.Surface.fill(screen, (0,0,0))
+        dt = 0.0
+        clock = pygame.time.Clock()
         pygame.display.flip()
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+        dt = clock.tick(MAX_FPS)/1000
             
 if __name__ == "__main__":
     main()
